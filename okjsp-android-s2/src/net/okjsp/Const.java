2f79e6bb52baa382a405c2989d5aca4dad5837a2
Jongin Oh <ohjongin@gmail.com>
2013-05-20 11:28:22 +0900

    Http Session 관리를 위한 NetworkManager 추가

--- okjsp-android-s2/src/net/okjsp/Const.java
+++ okjsp-android-s2/src/net/okjsp/Const.java
@@ -4,8 +4,10 @@ public interface Const {
 	public static final String BASE_URL = "http://okjsp.net";
 	public static final String MAIN_BOARD = "/bbs?act=FIRST_MAIN";
 	public static final String BBS_BOARD = "/bbs?act=LIST&bbs=";
+	public static final String LOGIN_PATH = "/jsp/member/login.jsp";
 	public static final String MAIN_BOARD_URL = BASE_URL + MAIN_BOARD;
 	public static final String BBS_BOARD_URL = BASE_URL + BBS_BOARD;
+	public static final String LOGIN_URL = BASE_URL + LOGIN_PATH;
 	
 	public static final String BOARD_URI_SCHEME = "board://";
 	public static final String IMAGE_CACHE_DIR = "cache";
@@ -15,4 +17,7 @@ public interface Const {
 	
 	public static final int MSG_PARSE_MAIN_PAGE_DONE = 1;
 	public static final int MSG_PARSE_BOARD_PAGE_DONE = 2;
+	
+	public static final String USERNAME = "id";
+	public static final String PASSWORD = "password";
 }
